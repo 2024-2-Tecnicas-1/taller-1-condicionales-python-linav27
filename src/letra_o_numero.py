fdce366c19bf5593c0dfc5ee9cd11b3da94fa2aa
Lina velasco <linam.velascod@konradlorenz.edu.co>
2024-09-06 15:03:34 -0500

    Letra o número

--- src/letra_o_numero.py
+++ src/letra_o_numero.py
@@ -1,6 +1,13 @@
 def evaluar(caracter):
-    # TODO: Coloca aquí el código del ejercicio 4: Letra o número
-    return "";
+    if caracter .isdigit():
+        return "Es número"
+    elif caracter.isalpha():
+        if caracter.isupper():
+            return "Es letra mayúscula"
+        else:
+            return "Es letra minúscula"
+    else:      
+     return "No es letra ni número"
 
 if __name__ == '__main__':
     print("Caracter:", end='')
