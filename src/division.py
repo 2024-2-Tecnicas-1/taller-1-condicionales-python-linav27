1f1b947bf8ca460991ea2596652d8a4ce6a582c1
Lina velasco <linam.velascod@konradlorenz.edu.co>
2024-09-06 14:31:15 -0500

    división

--- src/division.py
+++ src/division.py
@@ -1,10 +1,16 @@
 def evaluar(dividendo, divisor):
-    # TODO: Coloca aquí el código del ejercicio 3: Division
-    cociente = 0
-    residuo = 0
-    respuesta = "La división es exacta. \n" \
-            "Cociente: " + str(cociente) + "\n" \
-            "Residuo: " + str(residuo)
+    if dividendo % divisor == 0:
+        cociente = dividendo // divisor
+        residuo = 0
+        respuesta = "La división es exacta. \n" \
+              "Cociente: " + str(cociente) + "\n" \
+                "Residuo: " + str(residuo)
+    else:
+        cociente = dividendo // divisor
+        residuo = dividendo % divisor
+        respuesta = "La división no es exacta. \n" \
+                "Cociente: " + str(cociente) + "\n" \
+                "Residuo: " + str(residuo)
     return respuesta
 
 if __name__ == '__main__':
