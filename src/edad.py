5d398337a4ba532dfbb7f06b80c20ca459967bc8
Lina velasco <linam.velascod@konradlorenz.edu.co>
2024-09-06 14:37:50 -0500

    Edad

--- src/edad.py
+++ src/edad.py
@@ -1,6 +1,14 @@
+from time import localtime
 def evaluar(dia, mes, anno):
-    # TODO: Coloca aquí el código del ejercicio 6: Edad
-    return "";
+    t = localtime()
+    diaA = t.tm_mday
+    mesA = t.tm_mon
+    annoA = t.tm_year
+    edad =annoA - anno - 1
+    if(mes > mesA) or (mes == mesA and dia > diaA)
+    else : 
+     annoA - anno 
+    return ""
 
 if __name__ == '__main__':
     print("Ingrese su fecha de nacimiento")
